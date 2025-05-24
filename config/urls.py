f1e4f29d23cab339177bdbfbf6efd7226feb3538
Mark Volkov <markasch67@gmail.com>
2025-05-24 21:06:29 +0300

    Last homework changes

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
-]
+] + static(settings.MEDIA_URL, docyment_root=settings.MEDIA_ROOT)
