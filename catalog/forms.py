8accc2dd1609138d33fa6c8db61bb486a90e32a5
Mark Volkov <markasch67@gmail.com>
2025-06-17 16:46:56 +0300

    Fixed form return

--- catalog/forms.py
+++ catalog/forms.py
@@ -35,6 +35,7 @@ class ProductForm(FormControlMixin, forms.ModelForm):
                     'Слово "%(word)s" не должно содержаться в названии продукта.',
                     params={"word": word},
                 )
+        return name
 
     def clean_description(self):
         description = self.cleaned_data.get("description")
@@ -46,8 +47,12 @@ class ProductForm(FormControlMixin, forms.ModelForm):
                     params={"word": word},
                 )
 
+        return  description
+
     def clean_price(self):
         price = self.cleaned_data.get("price")
 
         if price < 0:
             raise ValidationError("Цена не может быть отрицательной.")
+
+        return price
